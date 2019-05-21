3110b2ef76128349bce1da315349b1bfe8e79b05
mobin <mobinyazdanparast(@gmail.com>
2019-05-21 18:58:31 +0430

    fix design

---
+++ src/components/AlbumItem.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		
+	</div>
+</template>
+
+<script>
+export default {
+	name:"components.video-item"
+};
+</script>
+
+<style>
+	
+</style>
---
+++ src/components/HomeAlbumsCarousel.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <carousel  :responsive="carousel_respansive" :margin="16" :freeDrag="false" :dots="false">
+      <AlbumItem :album="album"/>
+      <AlbumItem :album="album"/>
+      <AlbumItem :album="album"/>
+      <AlbumItem :album="album"/>
+      <AlbumItem :album="album"/>
+      <AlbumItem :album="album"/>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import carousel from "vue-owl-carousel";
+import AlbumItem from "@/components/AlbumItem";
+export default {
+  name: "components.home_albums_carousel",
+  components: {
+    AlbumItem,
+    carousel
+  },
+  data() {
+    return {
+      carousel_respansive: {
+        0: { items: 2, nav: false },
+        576: { items: 2, nav: false },
+        768: { items: 2, nav: false },
+        992: { items: 3, nav: false },
+        1200: { items: 3, nav: false }
+      },
+      album: {
+        id: "1",
+        title: "test music",
+        artist: {
+          name: "test artist"
+        },
+        image:
+          "https://assets.rj-a2.com/static/content_items/images/2019/05/20/957a605d7a92135.jpg",
+        link128: "sasd"
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.owl-stage-outer {
+  width: 100%;
+}
+</style>
